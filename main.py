20f3dc55eb329600f225535ebf69c70eccafa2b5
unknown <arunthathimurugan1979@gmail.com>
2025-12-16 23:21:28 +0530

    Add interactive CodeSensei chatbot with loop

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("CodeSensei project start!")
+print("🤖 Welcome to CodeSensei")
+
+while True:
+    question = input("Ask me a coding question (type exit to quit): ").lower()
+
+    if question == "exit":
+        print("CodeSensei: Bye! Happy coding 👋")
+        break
+
+    elif "python" in question:
+        print("CodeSensei: Python is a beginner-friendly programming language 🐍")
+
+    elif "c" in question:
+        print("CodeSensei: C is a powerful low-level programming language ⚙")
+
+    elif "embedded" in question:
+        print("CodeSensei: Embedded systems control hardware using software 🔌")
+
+    else:
+        print("CodeSensei: I am still learning this topic 😊")
